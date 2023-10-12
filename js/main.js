3ae5d35938acef2e881155f44cd546905bf88e61
Kriuchkovaea1 <79831087227@yandex.ru>
2023-10-12 12:24:58 +0700

    desctop Vr ended

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+const tabContent = document.querySelectorAll('.tabs__content-item');
+const tabsItem = document.querySelectorAll('.tabs__btn-item');
+
+tabsItem.forEach(function (el) {
+    el.addEventListener('click', open);
+})
+
+function open(evt) {
+    const tabTarget = evt.currentTarget;
+    const button = tabTarget.dataset.button;
+    tabsItem.forEach(function (item) {
+        item.classList.remove('tabs__btn-item--active');
+    })
+
+    tabTarget.classList.add('tabs__btn-item--active');
+    tabContent.forEach(function (item){
+        item.classList.remove('tabs__content-item--active')
+    });
+
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+}
+
 const swiper = new Swiper('.swiper', {
     loop: true,
 
